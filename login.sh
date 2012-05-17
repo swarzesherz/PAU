4d16e0de59e210102817affd897b3d442fe2357d
Herz <achwazer@gmail.com>
2012-05-17 13:18:44 -0500

    Update loginwq

--- login.sh
+++ login.sh
@@ -1,10 +1,44 @@
-#!/bin/sh
-
-auth=$(zenity --password --username)
+#!/bin/bash
+trap 'echo Saliendo... ; exit 1' 1 2 3 15 20
+authUser=$(zenity --entry \
+	--title="Usuario" \
+	--text="Escriba su correo electronico:")
 case $? in
          0)
-                zenity --info \
-		--text="Validadando"
+		if [ ! -f "${authUser:-NULL}.user.asc" ]
+			then
+				zenity --error \
+				--text="El usuario ${authUser:-NULL} no existe en el sistema"
+				exit 1
+		fi
+		;;
+         1)
+                zenity --error \
+		--text="No se pudo autenticar"
+		exit 1
+		;;
+        -1)
+                zenity --error \
+		--text="Ha ocurrido un error inesperado."
+		exit 1
+		;;
+esac
+
+authPasswd=$(zenity --entry \
+	--title="Password" \
+	--text="Escriba su password:" \
+	--hide-text)
+
+case $? in
+         0)
+		user="$(gpg --batch --passphrase "${authPasswd:-NULL}" -o - -q "${authUser:-NULL}.user.asc")"
+		if [ $? -ne 0 ]
+			then
+				zenity --error \
+				--text="Password es incorrecto"
+				exit 1
+		fi
+		echo $user
 		;;
          1)
                 zenity --error \
--- main.sh
+++ main.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-./login.sh
+user=$(./login.sh)
 if [ $? != 0 ]
 then
 	exit 1
 fi
+export user
+echo $user | awk 'BEGIN {FS=":"} {print $1}'
