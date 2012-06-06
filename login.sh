b2cca8fb5b911b05ec3e9f9bd8951fb8852415c1
Herz <achwazer@gmail.com>
2012-06-06 13:56:49 -0500

    Login actliado (uso de DB)

--- login.sh
+++ login.sh
@@ -1,44 +1,30 @@
 #!/bin/bash
 trap 'echo Saliendo... ; exit 1' 1 2 3 15 20
-authUser=$(zenity --entry \
-	--title="Usuario" \
-	--text="Escriba su correo electronico:")
-case $? in
-         0)
-		if [ ! -f "${authUser:-NULL}.user.asc" ]
-			then
-				zenity --error \
-				--text="El usuario ${authUser:-NULL} no existe en el sistema"
-				exit 1
-		fi
-		;;
-         1)
-                zenity --error \
-		--text="No se pudo autenticar"
-		exit 1
-		;;
-        -1)
-                zenity --error \
-		--text="Ha ocurrido un error inesperado."
-		exit 1
-		;;
-esac
 
-authPasswd=$(zenity --entry \
-	--title="Password" \
-	--text="Escriba su password:" \
-	--hide-text)
+auth=$(zenity --forms --title="Iniciar sesion" \
+	--text="Intro" \
+	--separator="|" \
+	--add-entry="Correo-e" \
+	--add-password="Password")
 
 case $? in
          0)
-		user="$(gpg --batch --passphrase "${authPasswd:-NULL}" -o - -q "${authUser:-NULL}.user.asc")"
-		if [ $? -ne 0 ]
-			then
+			email=$(echo ${auth:-NULL} | awk 'BEGIN{FS="|"; ORS="";}{print $1;}') 
+			password=$(echo ${auth:-NULL} | awk 'BEGIN{FS="|"; ORS="";}{print $2;}' | md5sum | awk '{print $1}')
+			user=$(psql -h localhost -p 5432 -U postgres burgerking -A -t -F ':' -c "SELECT vendedor_id, ap_pat, ap_mat, nombre, passwd FROM vendedor WHERE vendedor_correo='${email}'")
+			if [ $? -ne 0 -o ${user:-NULL} == "NULL" ]
+				then
 				zenity --error \
-				--text="Password es incorrecto"
+				--text="El usuario ${email:-NULL} no existe en el sistema"
 				exit 1
-		fi
-		echo $user
+			fi
+			if [ ${password:-NULL} != $(echo ${user:-NULL} | awk 'BEGIN{FS=":"; ORS="";}{print $5;}') ]
+				then
+				zenity --error \
+				--text="Password incorrecto"
+				exit 1
+			fi
+			echo $user;
 		;;
          1)
                 zenity --error \
